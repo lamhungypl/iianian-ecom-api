ba3e53497f049e012a06957f034f9586da9c208d
lamhung <lamhungypl@gmail.com>
2020-07-22 09:03:41 +0700

    commit

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,18 @@
  * Licensed under the MIT license.
  */
 
-import 'reflect-metadata';
-import { bootstrapMicroframework } from 'microframework-w3tec';
-import { banner } from './lib/banner';
-import { Logger } from './lib/logger';
-import { eventDispatchLoader } from './loaders/eventDispatchLoader';
-import { expressLoader } from './loaders/expressLoader';
-import { homeLoader } from './loaders/homeLoader';
-import { iocLoader } from './loaders/iocLoader';
-import { monitorLoader } from './loaders/monitorLoader';
-import { publicLoader } from './loaders/publicLoader';
-import { typeormLoader } from './loaders/typeormLoader';
-import { winstonLoader } from './loaders/winstonLoader';
+import "reflect-metadata";
+import { bootstrapMicroframework } from "microframework-w3tec";
+import { banner } from "./lib/banner";
+import { Logger } from "./lib/logger";
+import { eventDispatchLoader } from "./loaders/eventDispatchLoader";
+import { expressLoader } from "./loaders/expressLoader";
+import { homeLoader } from "./loaders/homeLoader";
+import { iocLoader } from "./loaders/iocLoader";
+import { monitorLoader } from "./loaders/monitorLoader";
+import { publicLoader } from "./loaders/publicLoader";
+import { typeormLoader } from "./loaders/typeormLoader";
+import { winstonLoader } from "./loaders/winstonLoader";
 
 /**
  * EXPRESS TYPESCRIPT BOILERPLATE
@@ -44,8 +44,8 @@ bootstrapMicroframework({
         expressLoader,
         monitorLoader,
         homeLoader,
-        publicLoader,
-    ],
+        publicLoader
+    ]
 })
     .then(() => banner(log))
-    .catch(error => log.error('Application is crashed: ' + error));
+    .catch(error => log.error("Application is crashed: " + error));
