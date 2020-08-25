1d0a975cea0a7d3377eb7b43e7f74424d574289c
lamhung <lamhungypl@gmail.com>
2020-08-25 15:02:09 +0700

    new api docs

--- src/public/apidoc/api_project.js
+++ src/public/apidoc/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "ecommerce ",  "version": "1.0.0",  "description": "RESTFUL API Document",  "title": "RESTFUL API Document",  "url": "http://localhost:8000",  "sampleUrl": "http://localhost:8000",  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2020-07-31T17:31:30.460Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "ecommerce ",
+  "version": "1.0.0",
+  "description": "RESTFUL API Document",
+  "title": "RESTFUL API Document",
+  "url": "http://localhost:8000",
+  "sampleUrl": "http://localhost:8000",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-08-25T07:44:12.563Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
