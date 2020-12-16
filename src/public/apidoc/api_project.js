8f3bbfaf18ea57bc21158aca78b99948c6ff1c50
lamhung <lamhungypl@gmail.com>
2020-12-16 09:40:27 +0700

    prettier ignore apidoc folder

--- src/public/apidoc/api_project.js
+++ src/public/apidoc/api_project.js
@@ -1,16 +1,16 @@
 define({
-  name: 'ecommerce ',
-  version: '1.0.0',
-  description: 'RESTFUL API Document',
-  title: 'RESTFUL API Document',
-  url: 'http://localhost:8000',
-  sampleUrl: 'http://localhost:8000',
-  defaultVersion: '0.0.0',
-  apidoc: '0.3.0',
-  generator: {
-    name: 'apidoc',
-    time: '2020-12-15T09:39:14.151Z',
-    url: 'https://apidocjs.com',
-    version: '0.25.0',
-  },
+  "name": "ecommerce ",
+  "version": "1.0.0",
+  "description": "RESTFUL API Document",
+  "title": "RESTFUL API Document",
+  "url": "http://localhost:8000",
+  "sampleUrl": "http://localhost:8000",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-12-16T02:18:22.741Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
 });
